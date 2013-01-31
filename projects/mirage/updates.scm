28eb54c3b5bc0464602da9d644527abe57c37743
Amir Chaudhry <amirmc@gmail.com>
2013-01-31 13:17:52 +0000

    add linebreaks

--- projects/mirage/updates.scm
+++ projects/mirage/updates.scm
@@ -1,17 +1,25 @@
 (
     (date 2012-11-10)
     (ty (Accepted "asplos2013"))
-    (descr "The Mirage paper on the low-level implementation details of the Xen backend was accepted at ASPLOS. The team is currently working on revising the paper for camera-ready (and tagging the release to include it in the final version, to encourage reproducable results).")
+    (descr "The Mirage paper on the low-level implementation details of the Xen backend was accepted at ASPLOS.
+      The team is currently working on revising the paper for camera-ready (and tagging the release to include
+        it in the final version, to encourage reproducable results).")
 )
 (
   (date 2012-08-27)
   (ty (Event "xensummit-2012"))
-  (descr "Anil Madhavapeddy described the low-level portions of Mirage and how they map onto the Xen interfaces, discussed future directions, and showed off the 100KB self-hosted microkernel presentation. The [Bromium](http://bromium.com) team were interested in looking at this on their microvisor, and datacenter folks wanted to know if this would help with lightweight datacenter proxies.")
+  (descr "Anil Madhavapeddy described the low-level portions of Mirage and how they map onto the Xen interfaces,
+    discussed future directions, and showed off the 100KB self-hosted microkernel presentation. The
+    [Bromium](http://bromium.com) team were interested in looking at this on their microvisor, and datacenter
+    folks wanted to know if this would help with lightweight datacenter proxies.")
 )
 (
   (date 2012-09-25)
   (ty (Event "xensummit-2012"))
-  (descr "Anil Madhavapeddy delivered roughly the same XenSummit talk at the Princeton departmental seminar. There was a *lot* of interest in the software-defined networking libraries, as Princeton has major efforts ongoing to apply high-level programming discipline to network construction. Andrew Appel also described his ongoing Compcert efforts and how Mirage could fit into this in the longer-term.")
+  (descr "Anil Madhavapeddy delivered roughly the same XenSummit talk at the Princeton departmental seminar.
+    There was a *lot* of interest in the software-defined networking libraries, as Princeton has major efforts
+    ongoing to apply high-level programming discipline to network construction. Andrew Appel also described his
+    ongoing Compcert efforts and how Mirage could fit into this in the longer-term.")
 )
 (
   (date 2012-09-14)
@@ -27,7 +35,8 @@
 (
   (date 2012-11-28)
   (ty (Event "xen-incubate-meeting"))
-  (descr "Anil, Dave and Amir are working with Lars (the xen.org community manager) to incorporate Mirage as an incubated project,
+  (descr "Anil, Dave and Amir are working with Lars (the xen.org community manager) to incorporate Mirage as an
+    incubated project,
     such that it can be officially supported by the Xen community.  The
     necessary paperwork should be ready soon, after which the proposal will be
     reviewed and voted on by the Xen community.")
@@ -42,12 +51,17 @@
 (
   (date 2012-12-05)
   (ty (Event "techmesh-talk"))
-  (descr "Dave Scott explained the background behind using OCaml in the Xen Cloud Platform, and demonstrated the integration of Mirage and Xen in a future update.  His slides were [self-hosted](http://github.com/djs55/techmesh-2012) using a Mirage webserver running Xen on his laptop.")
+  (descr "Dave Scott explained the background behind using OCaml in the Xen Cloud Platform, and demonstrated the
+    integration of Mirage and Xen in a future update.  His slides were [self-hosted](http://github.com/djs55/techmesh-2012)
+    using a Mirage webserver running Xen on his laptop.")
 )
 (
   (date 2012-12-06)
   (ty (Event "birmingham-talk"))
-  (descr "Anil Madhavapeddy delivered the CS departmental seminar about the state of Mirage, based on the forthcoming ASPLOS 2013 paper results. Since OCaml is [taught](https://sites.google.com/site/focs1112/lecture-videos) to the undergrads, there was an unusually knowledgeable audience! [Dan Ghica](http://www.cs.bham.ac.uk/~drg/) also explained his [GoS](https://sites.google.com/site/thegeometryofsynthesis/) compiler which is relevant to the LinearML efforts.")
+  (descr "Anil Madhavapeddy delivered the CS departmental seminar about the state of Mirage, based on the forthcoming
+    ASPLOS 2013 paper results. Since OCaml is [taught](https://sites.google.com/site/focs1112/lecture-videos) to the
+    undergrads, there was an unusually knowledgeable audience! [Dan Ghica](http://www.cs.bham.ac.uk/~drg/) also explained his
+    [GoS](https://sites.google.com/site/thegeometryofsynthesis/) compiler which is relevant to the LinearML efforts.")
 )
 (
   (date 2013-01-10)
