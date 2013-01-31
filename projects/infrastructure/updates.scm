94e76007274fb9c0120cc7bce7a2fbc546218402
Amir Chaudhry <amirmc@gmail.com>
2013-01-31 13:11:31 +0000

    add new mailing list info

--- projects/infrastructure/updates.scm
+++ projects/infrastructure/updates.scm
@@ -51,3 +51,14 @@
           [here TODO](#).  One more round of itereation will follow in the coming weeks so
           please do get in touch with Amir if you have feedback.")
 )
+(
+  (date 2012-12-04)
+  (ty (Event "mailing-lists"))
+  (descr
+    "A new working group mailing list has been created regarding the future of syntax extensions in OCaml (wg-camlp4).
+    The aim of the working group is to formulate a solid transition plan to create a 'basic OCaml cosystem' that does
+    not require camlp4.  Alain Frisch's introductory email has more detail and can be found
+     [in the archive](http://lists.ocaml.org/pipermail/wg-camlp4/2013-January/000000.html).  Working groups are intended
+     to be community driven projects and the intent is to archive the list once the projects are complete.
+     Two other lists are also planned, which are multicore working group and the ongoing platform list.")
+)
