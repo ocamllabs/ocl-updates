314e4ea1bbadaa04204830e49dc5195a00e77c07
Amir Chaudhry <amirmc@gmail.com>
2013-02-25 15:13:55 +0000

    line breaks

--- projects/mirage/outputs.scm
+++ projects/mirage/outputs.scm
@@ -4,11 +4,21 @@
         (link (Webpage "http://openmirage.org"))))
   (ty (Paper ((name "Eighteenth International Conference on Architectural Support for Programming Languages and Operating Systems (ASPLOS)")
               (link (Webpage "http://asplos13.rice.edu")) )))
-  (descr "We advocate *unikernels*: specialized virtual machines that run high-level languages directly on an OS hypervisor. Unikernels build on the concept of library operating systems, where a conventional OS is replaced with application libraries that implement kernel services such as device drivers. The traditional barrier to wide-scale use of library OSs has been hardware support and isolation between applications: both problems that we solve by running unikernels on a hypervisor that exposes a stable virtual hardware interface.
+  (descr "We advocate *unikernels*: specialized virtual machines that run high-level languages directly on an OS hypervisor.
+    Unikernels build on the concept of library operating systems, where a conventional OS is replaced with application libraries that implement
+    kernel services such as device drivers. The traditional barrier to wide-scale use of library OSs has been hardware support and isolation
+    between applications: both problems that we solve by running unikernels on a hypervisor that exposes a stable virtual hardware interface.
 
-Our Mirage implementation produces unikernels that offer an order of magnitude reduction in code size and smaller VM memory footprints without significant performance penalty, and can run on commodity cloud deployments such as Amazon's EC2. The library-based architecture enables security, efficiency and performance optimisations, such as a single-address space that can be made immutable via an optional hypervisor extension, and compile-time configuration specialisation and inlining. \mirage{} is written entirely in a statically type-safe functional language, and the resulting unikernels are protected against low-level memory errors, and made convenient to program via high-level interfaces.
+    Our Mirage implementation produces unikernels that offer an order of magnitude reduction in code size and smaller VM memory footprints without
+    significant performance penalty, and can run on commodity cloud deployments such as Amazon's EC2. The library-based architecture enables security,
+    efficiency and performance optimisations, such as a single-address space that can be made immutable via an optional hypervisor extension, and
+    compile-time configuration specialisation and inlining. \mirage{} is written entirely in a statically type-safe functional language, and the
+    resulting unikernels are protected against low-level memory errors, and made convenient to program via high-level interfaces.
 
-Mirage also provides a suite of clean-slate, type-safe protocol libraries including OpenFlow, TCP/IP, DNS, SSH and HTTP. We find that the overall performance of these unikernels matches or surpasses the conventional Linux VM equivalents. Our results comprehensively demonstrate that the OS hypervisor is a practical platform to overcome the hardware compatibility issues that have made past library operating systems difficult to deploy in the real-world.")
+    Mirage also provides a suite of clean-slate, type-safe protocol libraries including OpenFlow, TCP/IP, DNS, SSH and HTTP. We find that the overall
+    performance of these unikernels matches or surpasses the conventional Linux VM equivalents. Our results comprehensively demonstrate that the OS
+    hypervisor is a practical platform to overcome the hardware compatibility issues that have made past library operating systems difficult to deploy
+    in the real-world.")
 )
 (
   (id "xensummit-2012")
@@ -46,7 +56,8 @@ Mirage also provides a suite of clean-slate, type-safe protocol libraries includ
   (extra (
      ((name Slides) (link (Pdf "http://wiki.freebsd.org/201210DevSummit?action=AttachFile&do=get&target=mirage-kfreebsd.pdf")))
   ))
-    (descr "This invitation-only FreeBSD Developer Summit took place at EuroBSDcon 2012, located in Warsaw, Poland. The developer summit spans three days, with a mixture of hacking, working group meetings, and presentations.")
+    (descr "This invitation-only FreeBSD Developer Summit took place at EuroBSDcon 2012, located in Warsaw, Poland.
+      The developer summit spans three days, with a mixture of hacking, working group meetings, and presentations.")
 )
 (
   (id "xen-incubate-meeting")
@@ -65,9 +76,17 @@ Mirage also provides a suite of clean-slate, type-safe protocol libraries includ
   (extra (
      ((name Slides) (link (Github (djs55 techmesh-2012))))
   ))
-  (descr "XenServer is an open source virtualization platform that powers some of the world's biggest enterprises, and drives the biggest clouds. A little known fact about XenServer is that it's central management server is written in OCaml. But how did this come about, you say? Why did we choose to write a critical, systems-level hypervisor management server in a high-level, functional language? From its humble beginnings in use as a document generator for our API, OCaml has served us well. It's quick compile times, fast native code and good C interface, strong static types and type inference, and even objects and classes, make it a great langauge for systems programming.
+  (descr "XenServer is an open source virtualization platform that powers some of the world's biggest enterprises, and drives the biggest clouds.
+    A little known fact about XenServer is that it's central management server is written in OCaml. But how did this come about, you say?
+    Why did we choose to write a critical, systems-level hypervisor management server in a high-level, functional language? From its humble
+    beginnings in use as a document generator for our API, OCaml has served us well. It's quick compile times, fast native code and good C
+    interface, strong static types and type inference, and even objects and classes, make it a great langauge for systems programming.
 
-In this talk, we'll give an overview of XenServer's unique hypervisor architecture, and our future plans to push OCaml even further down the stack and create server applications that run on bare metal, with no operating system between them and the hypervisor. We'll talk about all the features that make a OCaml a great langauge to use, as well as interesting new features we'd like to make use of in our software. And we'd like to remind everyone that it's possible for a small startup to write enterprise software in a non-mainstream language, and still get acquired by a big multinational.")
+    In this talk, we'll give an overview of XenServer's unique hypervisor architecture, and our future plans to push OCaml even further down the stack
+    and create server applications that run on bare metal, with no operating system between them and the hypervisor. We'll talk about all the featurest
+    hat make a OCaml a great langauge to use, as well as interesting new features we'd like to make use of in our software. And we'd like to remind
+    everyone that it's possible for a small startup to write enterprise software in a non-mainstream language, and still get acquired by a big
+    multinational.")
 )
 (
   (id "birmingham-talk")
@@ -75,11 +94,19 @@ In this talk, we'll give an overview of XenServer's unique hypervisor architectu
         (link (Webpage "https://www.cs.bham.ac.uk/events/seminars/seminar_details.html?seminar_id=941")) ))
   (ty (Talk ((name "University of Birmingham Computer Science")
              (link (Webpage "https://www.cs.bham.ac.uk/")) )))
-  (descr "Public compute clouds provide a flexible platform to host applications as a set of appliances, e.g., web servers or databases. Each appliance usually contains an OS kernel and userspace processes, within which applications access resources via APIs such as POSIX. The flexible architecture of the cloud comes at a cost: the addition of another layer in the already complex software stack. This reduces performance and increases the size of the trusted computing base.
+  (descr "Public compute clouds provide a flexible platform to host applications as a set of appliances, e.g., web servers or databases.
+    Each appliance usually contains an OS kernel and userspace processes, within which applications access resources via APIs such as POSIX.
+    The flexible architecture of the cloud comes at a cost: the addition of another layer in the already complex software stack. This reduces
+    performance and increases the size of the trusted computing base.
 
-Our new Mirage operating system proposes a radically different way of building these appliances. Mirage supports the progressive specialisation of functional language (OCaml) application source code, and gradually replaces traditional OS components with type-safe libraries. This ultimately results in *unikernels*: sealed, fixed-purpose images that run directly on the hypervisor without an intervening guest OS such as Linux.
+    Our new Mirage operating system proposes a radically different way of building these appliances. Mirage supports the progressive specialisation
+    of functional language (OCaml) application source code, and gradually replaces traditional OS components with type-safe libraries. This ultimately
+    results in *unikernels*: sealed, fixed-purpose images that run directly on the hypervisor without an intervening guest OS such as Linux.
 
-Developers no longer need to become sysadmins, expert in the configuration of all manner of system components, to use cloud resources. At the same time, they can develop their code using their usual tools, only making the final push to the cloud once they are satisfied their code works. As they explicitly link in components that would normally be provided by the host OS, the resulting unikernels are also highly compact: facilities that are not used are simply not included in the resulting unikernel. For example, the self-hosting Mirage web server image is less than a megabyte in size!")
+    Developers no longer need to become sysadmins, expert in the configuration of all manner of system components, to use cloud resources. At the same time,
+    they can develop their code using their usual tools, only making the final push to the cloud once they are satisfied their code works. As they explicitly
+    link in components that would normally be provided by the host OS, the resulting unikernels are also highly compact: facilities that are not used are
+    simply not included in the resulting unikernel. For example, the self-hosting Mirage web server image is less than a megabyte in size!")
 )
 (
   (id "hotos2013")
@@ -87,5 +114,8 @@ Developers no longer need to become sysadmins, expert in the configuration of al
         (link (Pdf "http://anil.recoil.org/papers/drafts/2013-hotos-draft.pdf"))))
   (ty (Paper ((name "14th Workshop on Hot Topics in Operating Systems (HotOS)")
               (link (Webpage "https://www.usenix.org/conference/hotos13")) )))
-  (descr "Data centers have accidentally fulfilled the microkernel vision: they move critical operating system components into user-space middleware. Consequentially, it is time to move to a different view of the OS in the data center— treating the node OS merely as a local enforcement agent of global decisions made by user-space systems soft- ware. Based on this observation, we present a novel distributed OS design for treating the data center as a “warehouse-scale computer”, and explain why this has attractive benefits for common distributed applications.")
+  (descr "Data centers have accidentally fulfilled the microkernel vision: they move critical operating system components into user-space middleware.
+    Consequentially, it is time to move to a different view of the OS in the data center— treating the node OS merely as a local enforcement agent of
+    global decisions made by user-space systems soft- ware. Based on this observation, we present a novel distributed OS design for treating the data
+    center as a “warehouse-scale computer”, and explain why this has attractive benefits for common distributed applications.")
 )
