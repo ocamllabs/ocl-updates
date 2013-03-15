5fe74f6c2cbed1771e89cacf1e60de926bad68fd
Amir Chaudhry <amirmc@gmail.com>
2013-03-15 11:11:42 +0000

    add philippe to illuminate

--- projects/illuminate/project.scm
+++ projects/illuminate/project.scm
@@ -5,6 +5,7 @@
   (team (
     ((person "ileslie") (role "Project Lead") (since "2012-"))
     ((person "avsm") (role "") (since "2012-"))
+    ((person "pwang") (role "") (since "2013-"))
     ((person "achaudhry") (role "") (since "2012-"))
   ))
 )
