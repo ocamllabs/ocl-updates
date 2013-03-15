45c3daa43d5135fe752a4cce144a56f22ad4747b
Amir Chaudhry <amirmc@gmail.com>
2013-03-15 11:14:21 +0000

    add jeremy and thomas to irminsule

--- projects/irminsule/project.scm
+++ projects/irminsule/project.scm
@@ -5,6 +5,8 @@
   (team (
     ((person "avsm") (role "Project Lead") (since "2012-"))
     ((person "dscott") (role "Project sponsor at Citrix") (since "2012-"))
+    ((person "jyallop") (role "") (since "2013-"))
+    ((person "tgazagnaire") (role "") (since "2013-"))
     ((person "rproust") (role "Plan 9 lover") (since "2012-"))
   ))
 )
