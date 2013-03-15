979f8ab55021ce14f3070ea0e042d53b69c3440a
Amir Chaudhry <amirmc@gmail.com>
2013-03-15 11:12:41 +0000

    add jeremy to ocaml.org

--- projects/infrastructure/project.scm
+++ projects/infrastructure/project.scm
@@ -6,6 +6,7 @@
     ((person "avsm") (role "infrastructure project lead") (since "2012-"))
     ((person "aagarwal") (role "www project lead") (since "2011-"))
     ((person "ctroestler") (role "content") (since "2011-"))
+    ((person "jyallop") (role "OCamlot") (since "2013-"))
     ((person "achaudhry") (role "design") (since "2012-"))
     ((person "tgazagnaire") (role "packaging") (since "2012-"))
     ((person "lwhite") (role "repositories") (since "2012-"))
